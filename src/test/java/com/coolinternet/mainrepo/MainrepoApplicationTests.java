595aa0d08b15a28401995e6c0dc199f02b5c973e
2quarius <863115889@qq.com>
2019-05-29 14:10:39 +0800

    set up euraka server

---
+++ src/test/java/com/coolinternet/mainrepo/MainrepoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.coolinternet.mainrepo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MainrepoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
