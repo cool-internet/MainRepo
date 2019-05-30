3011e7e8abc9d0b39fd585ac63a5fe4e64afc7cf
2quarius <863115889@qq.com>
2019-05-30 21:37:42 +0800

    add feign

--- src/main/java/com/coolinternet/ribbon/service/BookstoreService.java
+++ src/main/java/com/coolinternet/ribbon/service/BookstoreService.java
@@ -1,15 +1,20 @@
 package com.coolinternet.ribbon.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-public class BookstoreService {
-    @Autowired
-    RestTemplate restTemplate;
-
-    public String getStorageContent(){
-        return restTemplate.getForObject("http://service-bookstore/storages",String.class);
-    }
+@FeignClient(value = "service-bookstore")
+public interface BookstoreService {
+    @GetMapping(value = "/storages")
+    String getStorageContent();
+//    @Autowired
+//    RestTemplate restTemplate;
+//
+//    public String getStorageContent(){
+//        return restTemplate.getForObject("http://service-bookstore/storages",String.class);
+//    }
 }
